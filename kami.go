c15c0d79275923878445a135ce0b36f38e126126
Tim Cooijmans <timcooijmans@gmail.com>
2015-08-08 11:32:31 +0200

    Make kami use a server struct while almost keeping backwards compatibility.

--- kami.go
+++ kami.go
@@ -3,72 +3,129 @@ package kami
 import (
 	"net/http"
 
+	"github.com/go-kami/tree"
 	"github.com/julienschmidt/httprouter"
 	"github.com/zenazn/goji/web/mutil"
 	"golang.org/x/net/context"
 )
 
-var (
-	// Context is the root "god object" from which every request's context will derive
-	Context = context.Background()
+// Server holds the state for a kami server. A new struct should be used per
+// server. This is not threadsafe.
+type Server struct {
+	BaseContext context.Context
 
-	// PanicHandler will, if set, be called on panics.
-	// You can use kami.Exception(ctx) within the panic handler to get panic details.
 	PanicHandler HandlerType
-	// LogHandler will, if set, wrap every request and be called at the very end.
-	LogHandler func(context.Context, mutil.WriterProxy, *http.Request)
-)
 
-var routes = httprouter.New()
+	LogHandler func(context.Context, mutil.WriterProxy, *http.Request)
+
+	routes *httprouter.Router
+
+	middleware map[string][]Middleware
+	wildcardMW *tree.Node
+}
+
+// NewServer creates and initializes a new Server.
+func NewServer() *Server {
+	server := &Server{
+		BaseContext: context.Background(),
+		routes:      httprouter.New(),
+		middleware:  make(map[string][]Middleware),
+		wildcardMW:  new(tree.Node)}
+	server.NotFound(nil)
+	return server
+}
+
+// DefaultServer holds an by-default initialized Server that is used by the
+// methods that do not define a Server.
+var DefaultServer *Server
 
 func init() {
-	// set up the default 404 handler
-	NotFound(nil)
+	DefaultServer = NewServer()
+}
+
+// Handler returns an http.Handler serving registered routes for the server.
+func (s *Server) Handler() http.Handler {
+	return s.routes
 }
 
 // Handler returns an http.Handler serving registered routes.
 func Handler() http.Handler {
-	return routes
+	return DefaultServer.Handler()
+}
+
+// Handle registers an arbitrary method handler un the given path for the server.
+func (s *Server) Handle(method, path string, handler HandlerType) {
+	s.routes.Handle(method, path, s.bless(wrap(handler)))
 }
 
 // Handle registers an arbitrary method handler under the given path.
 func Handle(method, path string, handler HandlerType) {
-	routes.Handle(method, path, bless(wrap(handler)))
+	DefaultServer.routes.Handle(method, path, DefaultServer.bless(wrap(handler)))
+}
+
+// Get registers a GET handler under the given path for the server.
+func (s *Server) Get(path string, handler HandlerType) {
+	s.Handle("GET", path, handler)
 }
 
 // Get registers a GET handler under the given path.
 func Get(path string, handler HandlerType) {
-	Handle("GET", path, handler)
+	DefaultServer.Get(path, handler)
+}
+
+// Post registers a POST handler under the given path
+func (s *Server) Post(path string, handler HandlerType) {
+	s.Handle("POST", path, handler)
 }
 
 // Post registers a POST handler under the given path.
 func Post(path string, handler HandlerType) {
-	Handle("POST", path, handler)
+	DefaultServer.Post(path, handler)
+}
+
+// Put registers a PUT handler under the given path for the server.
+func (s *Server) Put(path string, handler HandlerType) {
+	s.Handle("PUT", path, handler)
 }
 
 // Put registers a PUT handler under the given path.
 func Put(path string, handler HandlerType) {
-	Handle("PUT", path, handler)
+	DefaultServer.Put(path, handler)
+}
+
+// Patch registers a PATCH handler un the given path for the server.
+func (s *Server) Patch(path string, handler HandlerType) {
+	s.Handle("PATCH", path, handler)
 }
 
 // Patch registers a PATCH handler under the given path.
 func Patch(path string, handler HandlerType) {
-	Handle("PATCH", path, handler)
+	DefaultServer.Patch(path, handler)
+}
+
+// Head registers a HEAD handler under the given path for the server.
+func (s *Server) Head(path string, handler HandlerType) {
+	s.Handle("HEAD", path, handler)
 }
 
 // Head registers a HEAD handler under the given path.
 func Head(path string, handler HandlerType) {
-	Handle("HEAD", path, handler)
+	DefaultServer.Head(path, handler)
+}
+
+// Delete registers a DELETE handler under the given path for the server.
+func (s *Server) Delete(path string, handler HandlerType) {
+	s.Handle("DELETE", path, handler)
 }
 
 // Delete registers a DELETE handler under the given path.
 func Delete(path string, handler HandlerType) {
-	Handle("DELETE", path, handler)
+	DefaultServer.Delete(path, handler)
 }
 
 // NotFound registers a special handler for unregistered (404) paths.
 // If handle is nil, use the default http.NotFound behavior.
-func NotFound(handler HandlerType) {
+func (s *Server) NotFound(handler HandlerType) {
 	// set up the default handler if needed
 	// we need to bless this so middleware will still run for a 404 request
 	if handler == nil {
@@ -77,38 +134,44 @@ func NotFound(handler HandlerType) {
 		})
 	}
 
-	h := bless(wrap(handler))
-	routes.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	h := s.bless(wrap(handler))
+	s.routes.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h(w, r, nil)
 	})
+}
+
+// NotFound registers a special handler for unregistered (404) paths.
+// If handle is nil, use the default http.NotFound behavior.
+func NotFound(handler HandlerType) {
+	DefaultServer.NotFound(handler)
 }
 
 // bless is the meat of kami.
 // It wraps a HandleFn into an httprouter compatible request,
 // in order to run all the middleware and other special handlers.
-func bless(k ContextHandler) httprouter.Handle {
+func (s *Server) bless(k ContextHandler) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-		ctx := Context
+		ctx := s.BaseContext
 		if len(params) > 0 {
-			ctx = newContextWithParams(Context, params)
+			ctx = newContextWithParams(s.BaseContext, params)
 		}
 		ranLogHandler := false // track this in case the log handler blows up
 
 		writer := w
 		var proxy mutil.WriterProxy
-		if LogHandler != nil {
+		if s.LogHandler != nil {
 			proxy = mutil.WrapWriter(w)
 			writer = proxy
 		}
 
-		if PanicHandler != nil {
+		if s.PanicHandler != nil {
 			defer func() {
 				if err := recover(); err != nil {
 					ctx = newContextWithException(ctx, err)
-					wrap(PanicHandler).ServeHTTPContext(ctx, writer, r)
+					wrap(s.PanicHandler).ServeHTTPContext(ctx, writer, r)
 
-					if LogHandler != nil && !ranLogHandler {
-						LogHandler(ctx, proxy, r)
+					if s.LogHandler != nil && !ranLogHandler {
+						s.LogHandler(ctx, proxy, r)
 						// should only happen if header hasn't been written
 						proxy.WriteHeader(http.StatusInternalServerError)
 					}
@@ -116,27 +179,32 @@ func bless(k ContextHandler) httprouter.Handle {
 			}()
 		}
 
-		ctx, ok := run(ctx, writer, r)
+		ctx, ok := s.run(ctx, writer, r)
 		if ok {
 			k.ServeHTTPContext(ctx, writer, r)
 		}
 
-		if LogHandler != nil {
+		if s.LogHandler != nil {
 			ranLogHandler = true
-			LogHandler(ctx, proxy, r)
+			s.LogHandler(ctx, proxy, r)
 			// should only happen if header hasn't been written
 			proxy.WriteHeader(http.StatusInternalServerError)
 		}
 	}
 }
 
+func (s *Server) reset() {
+	s.BaseContext = context.Background()
+	s.PanicHandler = nil
+	s.LogHandler = nil
+	s.routes = httprouter.New()
+	s.middleware = make(map[string][]Middleware)
+	s.wildcardMW = new(tree.Node)
+	NotFound(nil)
+}
+
 // Reset changes the root Context to context.Background().
 // It removes every handler and all middleware.
 func Reset() {
-	Context = context.Background()
-	PanicHandler = nil
-	LogHandler = nil
-	middleware = make(map[string][]Middleware)
-	routes = httprouter.New()
-	NotFound(nil)
+	DefaultServer.reset()
 }
